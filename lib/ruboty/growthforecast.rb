a0da8b9c6ac01fbc38135ea89c80c4193810b1e6
mzp <mzpppp@gmail.com>
2015-04-03 21:21:13 +0900

    bundle gem ruboty-growthforecast

---
+++ lib/ruboty/growthforecast.rb
@@ -0,0 +1,7 @@
+require "ruboty/growthforecast/version"
+
+module Ruboty
+  module Growthforecast
+    # Your code goes here...
+  end
+end
---
+++ lib/ruboty/growthforecast/version.rb
@@ -0,0 +1,5 @@
+module Ruboty
+  module Growthforecast
+    VERSION = "0.1.0"
+  end
+end
